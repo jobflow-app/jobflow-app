fd3180fe1dd17620af2a6e569da17d1b8d88d885
JobFlow <info@vs-technik.de>
2026-03-13 21:17:59 +0100

    Update translations.js

--- components/lib/translations.js
+++ components/lib/translations.js
@@ -1,1 +1,40 @@
+export const translations = {
+
+  de: {
+    dashboard: "Dashboard",
+    jobs: "Aufträge",
+    customers: "Kunden",
+    invoices: "Rechnungen",
+    workers: "Mitarbeiter",
+    map: "Karte",
+    calendar: "Kalender",
+    reports: "Berichte",
+    settings: "Einstellungen"
+  },
+
+  en: {
+    dashboard: "Dashboard",
+    jobs: "Jobs",
+    customers: "Customers",
+    invoices: "Invoices",
+    workers: "Workers",
+    map: "Map",
+    calendar: "Calendar",
+    reports: "Reports",
+    settings: "Settings"
+  },
+
+  bhs: {
+    dashboard: "Dashboard",
+    jobs: "Poslovi",
+    customers: "Klijenti",
+    invoices: "Računi",
+    workers: "Radnici",
+    map: "Mapa",
+    calendar: "Kalendar",
+    reports: "Izvještaji",
+    settings: "Postavke"
+  }
+
+}
 

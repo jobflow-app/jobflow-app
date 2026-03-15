9a28239d4c5a3908c898a4b949539c3b9be6a741
JobFlow <info@vs-technik.de>
2026-03-16 00:57:38 +0100

    Create page.jsx

---
+++ app/admin/invoices/page.jsx
@@ -0,0 +1,8 @@
+export default function AdminInvoicesPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Rechnungen</h1>
+      <p>Alle Rechnungen und Zahlungen.</p>
+    </main>
+  )
+}

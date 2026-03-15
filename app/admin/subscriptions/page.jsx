7a129d4dedae698a4098899d5e6244d640348c4d
JobFlow <info@vs-technik.de>
2026-03-16 00:56:36 +0100

    Create page.jsx

---
+++ app/admin/subscriptions/page.jsx
@@ -0,0 +1,8 @@
+export default function AdminSubscriptionsPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Abos & Trial</h1>
+      <p>Pakete, Trial und Laufzeiten.</p>
+    </main>
+  )
+}

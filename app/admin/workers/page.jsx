505c10da3afe943a2ac0edf412a96cadfd1e8ae1
JobFlow <info@vs-technik.de>
2026-03-16 00:53:31 +0100

    Create page.jsx

---
+++ app/admin/workers/page.jsx
@@ -0,0 +1,8 @@
+export default function AdminWorkersPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Mitarbeiter</h1>
+      <p>Alle Mitarbeiter über alle Firmen.</p>
+    </main>
+  )
+}

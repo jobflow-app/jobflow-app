0e104f6d7f84a46b1b9f8e410b52e0e6c5037d90
JobFlow <info@vs-technik.de>
2026-03-15 01:02:59 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ export default function HomePage() {
       return
     }
 
-    if (userEmail === 'info@vs-technik.de') {
+    if (userEmail === 'velibor.savicc@gmail.com') {
       router.push('/admin')
       return
     }

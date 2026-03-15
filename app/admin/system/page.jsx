86e86a5cdfe04f21f3962f927e14b6231f950432
JobFlow <info@vs-technik.de>
2026-03-16 00:58:26 +0100

    Create page.jsx

---
+++ app/admin/system/page.jsx
@@ -0,0 +1,8 @@
+export default function AdminSystemPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>System</h1>
+      <p>Systemstatus, Logs und Sicherheit.</p>
+    </main>
+  )
+}

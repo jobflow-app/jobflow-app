93533f1a8c6224bb262a5322c80bd627a11d6216
JobFlow <info@vs-technik.de>
2026-03-16 02:01:27 +0100

    Create page.jsx

---
+++ app/superadmin/subscriptions/page.jsx
@@ -0,0 +1,8 @@
+export default function SubscriptionsPage() {
+  return (
+    <main style={{padding:40}}>
+      <h1>Abonnements</h1>
+      <p>Verwaltung der Firmen-Abonnements.</p>
+    </main>
+  )
+}

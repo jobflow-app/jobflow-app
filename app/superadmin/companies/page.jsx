e57ee71d82e39ac334191aa1163bb4c2c5230947
JobFlow <info@vs-technik.de>
2026-03-16 02:01:02 +0100

    Create page.jsx

---
+++ app/superadmin/companies/page.jsx
@@ -0,0 +1,8 @@
+export default function CompaniesPage() {
+  return (
+    <main style={{padding:40}}>
+      <h1>Firmen</h1>
+      <p>Hier siehst du alle Firmen im System.</p>
+    </main>
+  )
+}
